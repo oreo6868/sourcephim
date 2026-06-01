adb653802e5eb3175e408e814b5c91afe6a73f64
oreo6868 <levanha08112k@gmail.com>
2026-06-01 18:42:43 +0700

    Update Phim1080Provider.kt

--- Phim1080/src/main/kotlin/com/hexated/Phim1080Provider.kt
+++ Phim1080/src/main/kotlin/com/hexated/Phim1080Provider.kt
@@ -9,7 +9,7 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import org.jsoup.nodes.Element
 
 class Phim1080Provider : MainAPI() {
-    override var mainUrl = "https://phimmoichillea.pro/"
+    override var mainUrl = "https://motchilll.xyz/"
     override var name = "Phim1080"
     override val hasMainPage = true
     override var lang = "vi"
