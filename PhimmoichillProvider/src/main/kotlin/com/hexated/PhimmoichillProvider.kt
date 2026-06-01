d6a549febe5dc58b1331f78a387c08920295c54a
oreo6868 <levanha08112k@gmail.com>
2026-06-01 18:43:15 +0700

    Update PhimmoichillProvider.kt

--- PhimmoichillProvider/src/main/kotlin/com/hexated/PhimmoichillProvider.kt
+++ PhimmoichillProvider/src/main/kotlin/com/hexated/PhimmoichillProvider.kt
@@ -9,7 +9,7 @@ import java.net.URI
 import java.net.URLDecoder
 
 class PhimmoichillProvider : MainAPI() {
-    override var mainUrl = "https://phimmoichillea.pro/"
+    override var mainUrl = "https://motchilll.xyz/"
     private var directUrl = mainUrl
     override var name = "Phimmoichill"
     override val hasMainPage = true
